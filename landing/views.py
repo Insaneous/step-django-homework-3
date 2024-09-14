4327872c149a486a227a26e128a1271fb9a5a66b
Insaneous <35697279+Insaneous@users.noreply.github.com>
2024-09-14 17:50:36 +0500

    Add files via upload

--- landing/views.py
+++ landing/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+ctx = {
+    'footer': [{
+            'link': 'https://www.facebook.com/itstep.tashkent',
+            'img': 'https://tashkent.itstep.org/dist/images/src/images/academy/footer/f.svg',
+            'class': 'footer-new__link footer-new__link--fb'
+        },{
+            'link': 'https://www.instagram.com/itstep.tashkent/',
+            'img': 'https://tashkent.itstep.org/dist/images/src/images/academy/footer/ins.svg',
+            'class': 'footer-new__link footer-new__link--insta'
+        },{
+            'link': 'https://t.me/itstep_tashkent',
+            'img': 'https://tashkent.itstep.org/dist/images/src/images/icons/social-links/2.svg',
+            'class': 'footer-new__link footer-new__link--telega'
+        },]
+    }
+
 def index(request):
-    return render(request, 'landing/index.html')
+    return render(request, 'landing/index.html', ctx)
 
 def about(request):
-    return render(request, 'landing/about.html')
+    return render(request, 'landing/about.html', ctx)
 
 def contact(request):
-    return render(request, 'landing/contact.html')
+    return render(request, 'landing/contact.html', ctx)
