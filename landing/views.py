2c8b68fd1734e874520df3d19ca4a135ee61296f
Insaneous <35697279+Insaneous@users.noreply.github.com>
2024-09-14 17:36:57 +0500

    Add files via upload

---
+++ landing/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'landing/index.html')
+
+def about(request):
+    return render(request, 'landing/about.html')
+
+def contact(request):
+    return render(request, 'landing/contact.html')
